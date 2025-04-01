a6216389d535358348b8ef95c8e9e650b3bb486f
sosm4gic <sosmagic7@gmail.com>
2025-04-01 11:07:15 +0700

    cap nhat moi

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-export const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <main className="auth justify-center items-center ">{children}</main>;
-};
-
-export default Layout;
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return <main className="auth justify-center items-center">{children}</main>;
+}
