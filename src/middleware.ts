e9961178393b981a77004c91a32e83df1cbf1b27
sosm4gic <sosmagic7@gmail.com>
2025-04-01 23:45:45 +0700

    cap nhat them phan hook

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,24 +1,16 @@
-/* eslint-disable @typescript-eslint/no-unused-expressions */
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-// Định nghĩa danh sách các route công khai
-const isPublicRoute = createRouteMatcher([
-  "/sign-in(.*)",
-  "/sign-up(.*)",
-  "/api/webhooks/clerk", // ✅ Thêm API webhook vào danh sách public
-]);
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware((auth, req) => {
-  if (!isPublicRoute(req)) {
-    auth.protect();
+  // Cho phép truy cập công khai API webhook của Clerk
+  if (req.nextUrl.pathname === "/api/webhooks/clerk") {
+    return;
   }
+  auth();
 });
 
 export const config = {
   matcher: [
-    // Skip Next.js internals và static files
-    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
-    // Luôn chạy cho API routes
-    "/(api|trpc)(.*)",
+    "/((?!_next|.*\\..*).*)", // Loại bỏ file tĩnh và Next.js internals
+    "/(api|trpc)(.*)", // Chạy middleware trên API routes
   ],
 };
