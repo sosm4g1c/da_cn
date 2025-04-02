e033ed5dab790a4bf5f30fe5abc3285bcf09eae8
sosm4gic <sosmagic7@gmail.com>
2025-04-02 12:19:33 +0700

    cap nhat moi

--- src/lib/database/models/user.model.ts
+++ src/lib/database/models/user.model.ts
@@ -1,40 +1,43 @@
 import { Schema, model, models } from "mongoose";
 
-const UserSchema = new Schema({
-  clerkId: {
-    type: String,
-    required: true,
-    unique: true,
+const UserSchema = new Schema(
+  {
+    clerkId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    planId: {
+      type: Number,
+      default: 1,
+    },
   },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  photo: {
-    type: String,
-    required: true,
-  },
-  firstName: {
-    type: String,
-  },
-  lastName: {
-    type: String,
-  },
-  planId: {
-    type: Number,
-    default: 1,
-  },
-  creditBalance: {
-    type: Number,
-    default: 10,
-  },
-});
+  { timestamps: true }
+);
 
 const User = models?.User || model("User", UserSchema);
 
